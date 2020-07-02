d63e39fd5bc9417777c33e56808c30cb6f2acfb7
Alexande Becker <becker.alexander42@gmail.com>
2020-07-03 00:19:20 +0200

    changed the execution model of the program to properly handle the read and send threads. the read should now work (it compiles but not tested yet

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,50 @@
 extern crate moessbauer_data;
+extern crate crossbeam;
+use crossbeam::{channel, Sender, Receiver};
 use moessbauer_data::*;
 use std::thread;
 use std::net::{TcpListener, TcpStream, Shutdown};
-use std::io::{Read, Write};
+use std::io::{Read, Write, ErrorKind};
 use moessbauer_server::*;
+use std::fs::File;
+use std::path::Path;
 use std::env;
 use std::process;
 use std::str;
+use std::time::Duration;
 
-fn handle_client(mut stream: TcpStream) {
+fn main() {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    // set up the channel for the data_read thread to talk to the network threads
+    let (data_thread_tx, data_network_rx) = channel::unbounded();
+
+    let data_file = File::open(Path::new(&config.data_file_path)).unwrap();
+
+    // the peak data now has to be decoded and turned into the "peak" data structure
+    let data_thread_handle = thread::spawn(move || {
+        read_data(data_thread_tx, data_file)
+    });
+
+    // open the network socket and listen for connections
+    let listener = TcpListener::bind(config.socket).unwrap();
+    println!("Server listening on port {}", config.socket.port());
+
+    for stream in listener.incoming() {
+        let stream_data_rx = data_network_rx.clone();
+        let stream = stream.unwrap();
+        println!("New connection: {}", stream.peer_addr().unwrap());
+        thread::spawn(move || {
+            handle_client(stream, stream_data_rx);
+        });
+    }
+    let _ = data_thread_handle.join().unwrap();
+}
+
+fn handle_client(mut stream: TcpStream, data_stream: Receiver<MeasuredPeak>) {
     let mut data = [0 as u8; 50];
     while match stream.read(&mut data) {
         Ok(size) => {
@@ -23,44 +59,24 @@ fn handle_client(mut stream: TcpStream) {
             }
         },
         Err(_) => {
-            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+            println!("an error occurred, terminating connection with {}", stream.peer_addr().unwrap());
             stream.shutdown(Shutdown::Both).unwrap();
             false
         }
     } {}
 }
 
-fn main() {
-    let config = Config::new(env::args()).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
-
-    let test_data = read_test_data(&config.test_data_path[..]);
-    let peak_cnt = (test_data.len()/12) as usize;
-    println!("Read in {} test peaks", peak_cnt);
-    
-    // the peak data now has to be decoded and turned into the "peak" data structure
-    let mut test_peaks: Vec<MeasuredPeak> = Vec::new();
-    for i in 0..peak_cnt {
-        test_peaks.push(MeasuredPeak::new(&test_data[i*12..(i+1)*12]))
-    }
-
-
-    let listener = TcpListener::bind(config.socket).unwrap();
-    println!("Server listening on port {}", config.socket.port());
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("New connection: {}", stream.peer_addr().unwrap());
-                thread::spawn(move || {
-                    handle_client(stream)
-                });
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-            }
+fn read_data(data_tx: Sender<MeasuredPeak>, mut data_file: File) -> Result<(), std::io::Error> {
+    let mut raw_data = [0 as u8; 12];
+    let metadata = data_file.metadata()?;
+    if metadata.len() % 12 == 0 {
+        let peak_cnt = metadata.len()/12;
+        for _ in 0..peak_cnt {
+            data_file.read_exact(&mut raw_data[..])?;
+            let peak = MeasuredPeak::new(&raw_data);
+            data_tx.send(peak).unwrap();
+            thread::sleep(Duration::from_secs(1));
         }
     }
+    Ok(())
 }
